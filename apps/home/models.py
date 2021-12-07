74d02e20f775cdcda64cd43debbb1f0f8d8fe60a
snn <sinanm89@gmail.com>
2021-12-06 23:37:24 -0500

    working django

---
+++ apps/home/models.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+from django.db import models
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
